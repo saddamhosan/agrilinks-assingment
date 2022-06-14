75a5f626bb6f5664ff84164ebe0e60263d9e5873
saddamhosan <contact.saddamhosan.com>
2022-06-14 13:45:09 +0600

    added menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Menu from "./Component/Menu";
 
 function App() {
   return (
     <div>
+      <Menu/>
       <h1>hello</h1>
     </div>
   );
---
+++ src/Component/Menu.js
@@ -0,0 +1,12 @@
+
+const Menu = () => {
+    return (
+        <div className='flex justify-between bg-blue-500 text-white font-bold px-3'>
+            <h1></h1>
+            <h1>Profile</h1>
+            <h1>A</h1>
+        </div>
+    );
+};
+
+export default Menu;
